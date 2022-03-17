3d3b1c1ebef51a5a62e08f698848ec84c35ce931
Martin Funiak <ffuunnii@gmail.com>
2022-03-17 18:13:13 +0100

    Fixing build

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/app.module.css';
-import HorseList from './horselist';
-import RaceCard from './racecard';
-import HorseCard from './horsecard';
-import BetModal from './bet';
+import HorseList from '../src/horselist';
+import RaceCard from '../src/racecard';
+import HorseCard from '../src/horsecard';
+import BetModal from '../src/bet';
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
