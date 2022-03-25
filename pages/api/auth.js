5347f0a937facd5a8a5839ac9929050b09e99a28
Martin Funiak <ffuunnii@gmail.com>
2022-03-25 10:31:54 +0100

    Trying to fix broken deployment - ably authentication

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,13 +1,7 @@
 import Ably from "ably/promises";
 
 export default async function handler(req, res) {
-    // try {
-    //   const client = new Ably.Realtime(process.env.ABLY_API_KEY);
-    //   const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
-    //   res.status(200).json(tokenRequestData);
-    // } catch (error) {
-    //   res.status(200).send(error.stack);
-    // }
-
-    res.status(200).json({test: "test", ablykey: process.env.ABLY_API_KEY});
-  };
+    const client = new Ably.Realtime(process.env.ABLY_API_KEY);
+    const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
+    res.status(200).json(tokenRequestData);
+};
